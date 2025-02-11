725b420c3a1369faf9d9570ab7aa959b2901d67e
nvg79ru <nvg79ru@rambler.ru>
2025-02-11 18:01:14 +0200

    Почистил код

--- WebApiDowntime/Controllers/MacaddresstablesController.cs
+++ WebApiDowntime/Controllers/MacaddresstablesController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using WebApiDowntime.Models.NetworkDevices;
 
