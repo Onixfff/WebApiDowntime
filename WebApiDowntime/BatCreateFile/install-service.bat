46abb4fa107336c164202185ff18a4592569255d
Onixfff <danilnikonov01@mail.ru>
2025-03-18 17:35:23 +0200

    Добавил работу по релизам (чтобы он всегда работал на более новом) и к дополнению добавил путь который отсутствовал

--- WebApiDowntime/BatCreateFile/install-service.bat
+++ WebApiDowntime/BatCreateFile/install-service.bat
@@ -12,16 +12,30 @@ if %errorLevel% neq 0 (
 
 echo Installing WebApiDowntime Service...
 
-REM Путь к исполняемому файлу службы
-set SERVICE_PATH=%~dp0..\bin\Release\net8.0\WebApiDowntime.exe
+REM Поиск исполняемого файла
+set FOUND=0
+for %%v in (8.0 7.0 6.0) do (
+    if exist "%~dp0..\bin\Release\net%%v\win-x64\WebApiDowntime.exe" (
+        set SERVICE_PATH=%~dp0..\bin\Release\net%%v\WebApiDowntime.exe
+        set FOUND=1
+        goto :found
+    )
+)
 
-REM Проверяем существование файла
-if not exist "%SERVICE_PATH%" (
-    echo Error: Service executable not found at %SERVICE_PATH%
-    echo Please make sure the application is built in Release mode.
+:found
+if %FOUND% equ 0 (
+    echo Error: Service executable not found!
+    echo Please make sure that:
+    echo 1. The application is built in Release mode
+    echo 2. The executable is in bin\Release\net8.0\WebApiDowntime.exe
+    echo.
+    echo Current directory: %~dp0
+    echo Expected path: %~dp0..\bin\Release\net8.0\WebApiDowntime.exe
     pause
     exit /b 1
 )
+
+echo Found service executable at: %SERVICE_PATH%
 
 REM Проверяем, существует ли уже служба
 sc query "WebApiDowntime" >nul 2>&1
